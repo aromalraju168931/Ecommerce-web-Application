9044218625500c84fee7900921124a65e8b44c7f
AROMAL RAJU <aromalkovilakathuparambil@gmail.com>
2026-02-27 11:09:37 +0530

    Update settings.py

--- ecommerce_application/ecommerce_application/settings.py
+++ ecommerce_application/ecommerce_application/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gouthamvikhneshwaran@gmail.com'
-EMAIL_HOST_PASSWORD = 'csek abkj yeaj jygf'
+EMAIL_HOST_USER = 'example_mail@gmail.com'
+EMAIL_HOST_PASSWORD = 'aaaa bbbb cccc dddd'
